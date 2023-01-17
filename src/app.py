b978379ba5440a8bffac2911818b0d1ee6ee40a6
Dušan Maďar <madar.dusan@gmail.com>
2023-01-17 18:39:54 +0100

    Add yearly sections

--- src/app.py
+++ src/app.py
@@ -1,11 +1,11 @@
-from datetime import datetime
 import json
+from collections import defaultdict
+from datetime import datetime
 from pathlib import Path
 
 from flask import Flask, render_template
 from flask_htmlmin import HTMLMIN
 from flask_pretty import Prettify
-
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -24,8 +24,18 @@ HTMLMIN(app)
 def index():
     with open(Path(__file__).parent.parent.absolute().joinpath("data.json"), "r") as fd:
         data = json.load(fd)
+
+    yearly_data = defaultdict(dict)
+    for date, times in data.items():
+        year = date.split("-")[0]
+        yearly_data[int(year)][date] = times
+
+    now = datetime.now()
     return render_template(
-        "index.html", data=data, updated_at=datetime.now().isoformat(" ", "minutes")
+        "index.html",
+        yearly_data=yearly_data,
+        current_year=now.year,
+        updated_at=now.isoformat(" ", "minutes"),
     )
 
 

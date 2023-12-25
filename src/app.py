12f7845be2191dde766bd836114f92846b449293
Dušan Maďar <madar.dusan@gmail.com>
2023-12-25 23:26:58 +0100

    Use Bootstrap 5, add more stats

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 import json
 from collections import defaultdict
-from datetime import datetime
+from datetime import date, datetime
 from pathlib import Path
 
 from flask import Flask, render_template
@@ -26,14 +26,55 @@ def index():
         data = json.load(fd)
 
     yearly_data = defaultdict(dict)
-    for date, times in data.items():
-        year = date.split("-")[0]
-        yearly_data[int(year)][date] = times
+    for date_, times in data.items():
+        year = date_.split("-")[0]
+        yearly_data[int(year)][date_] = times
+
+    yearly_totals = {}
+    weekdays = ["Pondelok", "Utorok", "Streda", "Štvrtok", "Piatok", "Sobota", "Nedeľa"]
+
+    for year, daily_data in yearly_data.items():
+        months = set()
+        weekday_count = {weekday: 0 for weekday in weekdays}
+        for date_, data in daily_data.items():
+            months.add(date_.split("-")[1])
+            weekday = date.fromisoformat(date_).weekday()
+            weekday_count[weekdays[weekday]] += len(data)
+
+        total_count = 0
+        time_ranges = defaultdict(int)
+        for data in daily_data.values():
+            total_count += len(data)
+            for outage_start, _ in data:
+                if "00:00:00" <= outage_start <= "06:00:00":
+                    key= "night"
+                elif "06:00:01" <= outage_start <= "12:00:00":
+                    key = "morning"
+                elif "12:00:01" <= outage_start <= "18:00:00":
+                    key = "day"
+                elif "18:00:01" <= outage_start:
+                    key = "evening"
+
+                time_ranges[key] += 1
+
+        monthly_average = round(total_count / len(months), 2)
+        monthly_average = (
+            monthly_average if not monthly_average.is_integer() else int(monthly_average)
+        )
+        yearly_totals[year] = {
+            "days": len(daily_data),
+            "count": total_count,
+            "monthly_average": monthly_average,
+            "time_ranges": time_ranges,
+            "weekday_count": weekday_count,
+
+        }
 
     now = datetime.now()
     return render_template(
         "index.html",
         yearly_data=yearly_data,
+        yearly_totals=yearly_totals,
         current_year=now.year,
         updated_at=now.isoformat(" ", "minutes"),
     )
